6713d2ecfad53f2dc800c8b22dceca4804fbd06a
Aaron Voth <avoth07@gmail.com>
2023-03-05 15:34:04 -0500

    Primary Objective - Basic Functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 import { AppHeader } from "./AppHeader";
-import { PokemonListContainer } from "./PokemonListContainer";
+import PokemonList from "./PokemonListContainer";
 
 function App() {
   return (
     <div className="antialiased font-open-sans bg-gradient-to-b from-sky-100 to-sky-300 to-sky-400 min-h-screen">
       <AppHeader />
 
-      <div className="mx-auto py-5 w-3/6 flex flex-col gap-3">
+      <div className="mx-auto py-5 w-5/6 flex flex-col gap-3">
         <h2 className="font-bold text-2xl">Welcome to the PokeTracker!</h2>
         <p>
           Here is where you will be able to see which Pokemon you have left to
@@ -24,7 +24,7 @@ function App() {
           Use the search box below to search by a Pokemon's name or dex number.
         </p>
 
-        <PokemonListContainer />
+        <PokemonList/>
       </div>
     </div>
   );
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -8,6 +8,21 @@ module.exports = {
         'open-sans': ["'Open Sans'", 'sans-serif']
       },
     },
+    keyframes: {
+      wiggle: {
+        '0%, 100%': { transform: 'rotate(-25deg)' },
+        '50%': { transform: 'rotate(75deg)' },
+      },
+      eject: {
+        '0%': { transform: 'translate(-50%, -50%) scale(0)' },
+        '50%': { transform: 'translate(-50%, -50%) scale(1)' },
+        '100%': { transform: 'translate(-50%, -50%) scale(0)' },
+      },
+    },
+    animation: {
+      wiggle: 'wiggle .35s ease-in-out',
+      eject: 'eject 1s ease-out forwards',
+    }
   },
   plugins: [],
 };
