c82e76cc07b61ffac73dedb46c146259fa0b733a
Aaron Voth <avoth07@gmail.com>
2023-03-05 22:35:05 -0500

    minor styling adjustment for type-icons

--- client/src/PokemonListContainer.tsx
+++ client/src/PokemonListContainer.tsx
@@ -144,7 +144,7 @@ return (
 </div>
 {filteredList.map((pokemon: Pokemon) => (
   <div className='pokemon-card rounded-md flex items-center p-3' key={pokemon.dex_number} style={{ backgroundColor: pokemon.isCaught ? 'lightgreen' : 'white' }}>
-    <div id="pokeball" className="m-1">
+    <div id="pokeball" className="ml-1">
     <svg version="1.1" width="25" height="25" viewBox="0 0 25 25" role="button" className={pokemon.isCaught ?  "animate-wiggle" : ""} onClick={() => handleCatchPokemon(pokemon.dex_number)}>
           <path fill="#FFF" stroke="#000" strokeWidth="2" strokeMiterlimit="10" d="M24 12.5C24 18.85 18.85 24 12.5 24c-4.92 0-9.11-3.09-10.75-7.42l8.57-3.29c.31.9 1.17 1.54 2.18 1.54 1.28 0 2.33-1.05 2.33-2.33 0-.31-.061-.6-.17-.88l8.55-3.3c.51 1.3.79 2.7.79 4.18z"></path>
           <path fill={pokemon.isCaught ? "red" : "gray"} stroke="#000" strokeWidth="2" strokeMiterlimit="10" d="M23.21 8.32l-8.55 3.3a2.349 2.349 0 0 0-2.16-1.45c-1.28 0-2.33 1.05-2.33 2.33 0 .28.05.54.15.79l-8.57 3.29C1.26 15.31 1 13.94 1 12.5 1 6.15 6.15 1 12.5 1c4.88 0 9.05 3.04 10.71 7.32z"></path>
@@ -152,19 +152,21 @@ return (
         </svg>
     </div>
 
-    <div className="flex-1 text-center ml-5">
+    <div className="flex-1 text-center m-5">
       <p className='capitalize font-bold'>{pokemon.name}</p>
       <p className='font-bold'>#: {pokemon.dex_number}</p>
       <img className='pokemon-media mx-auto' src={pokemon.image_url} title={pokemon.name} />
-      <div className="flex justify-center">
-      <div className={classNames(styles['type-icon'], styles[String(pokemon.type_1)])}/>
+
+      <div className={classNames(styles['type-icon'], styles[String(pokemon.type_1)])} />      
       {/*hide type 2 icon if null*/}
       {pokemon.type_2 ? (
+
         <div className={classNames(styles['type-icon'], styles[String(pokemon.type_2)])} />
       ) : null}
-</div>
+      </div>
+    
     </div>
-  </div>
+  
 ))}
   </div>
 
